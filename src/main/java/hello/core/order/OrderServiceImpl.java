8379857a28b8028b0338fdefabaed452fabe7ade
moonyeju <moonyeju8510@gmail.com>
2023-09-19 14:11:28 +0900

    First commit 3. 스프링 핵심 원리 이해2 - 객체 지향 원리 적용

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -2,15 +2,27 @@ package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService{
 
-    private final MemberRepository memberRepository=new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy=new FixDiscountPolicy();
+//    private final DiscountPolicy discountPolicy=new FixDiscountPolicy();
+//    private final DiscountPolicy discountPolicy=new RateDiscountPolicy();
+    //이렇게 하면 실제 구현 코드를 바꿔야함. 결론적으로 추상도 의존하고 구체적인 것도 의존하고 있음. DIP어김.
+// FixDiscountPolicy 를 RateDiscountPolicy 로 변경하는 순간 OrderServiceImpl 의 소스코드도 함께 변경해야 한다! OCP 위반
+    private DiscountPolicy discountPolicy;
+    private MemberRepository memberRepository;
+    //생성자 주입
 
+    public OrderServiceImpl(DiscountPolicy discountPolicy, MemberRepository memberRepository) {
+        this.discountPolicy = discountPolicy;
+        this.memberRepository = memberRepository;
+    }
+
+    //인터페이스에만 의존하도록 변경함.
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
         Member member=memberRepository.findById(memberId); //회원 정보 조회
